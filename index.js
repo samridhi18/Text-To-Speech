744aa42d09c5627846dc1d7234999ffd0911c3c4
SAMRIDHI <53181511+samridhi18@users.noreply.github.com>
2020-06-17 21:59:06 +0530

    Project Added

---
+++ index.js
@@ -0,0 +1,10 @@
+function textToAudio(){
+let msg=document.getElementById("text-to-speech").value;
+let speech=new SpeechSynthesisUtterance();
+speech.lang="en-US";
+speech.text=msg;
+speech.volume=2;
+speech.rate=1;
+speech.pitch=1;
+window.speechSynthesis.speak(speech);
+}
